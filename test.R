7d710a0bdb07daafcb371ba4855199e3f6dcf729
Mat Kelly <machawk1@gmail.com>
2014-06-04 11:05:58 -0400

    Fixed axis, added final (prev missing) plot.

--- test.R
+++ test.R
@@ -1,7 +1,7 @@
 library (lattice)
 
 filename <- "testFull.txt"
-outputPath = "/Users/mkelly/Desktop/figure.png"
+outputPath = "/Users/mkelly/Desktop/NASALCRGIDL2R/figure.png"
 colores = c("purple","red","blue","orange")
 
 header_type <- scan(filename, nlines = 1, what = character())
@@ -21,11 +21,23 @@ names(data) <- headers
 png(filename=outputPath, height=1200, width=1200, bg="white", res=150) 
 par(mfrow=c(4,1))
 
-xal = paste("Date/time (",header_type[1],header_type[2],")",sep=" ")
+#xal = paste("Date/time (",header_type[1],header_type[2],")",sep=" ")
+xal <- "SITE-GMT (fra-dy)"
 
-for(i in as.single(3:6)) {
-  plot(data.frame(x=data[2],y=data[i]),pch=20,col=colores[i-2],lty=0,type="o",xlab=xal,ylab=header_type[i],
+
+for(i in as.single(4:7)) {
+  timeDecimal <- data[3] - floor(data[3]) #e.g., 280.9197 --> 0.9197
+  hour <- timeDecimal * 24 # preliminary, with decimal, e.g., 22.0728
+  minutesAsDecimal <- hour - floor(hour) # e.g., 0.0728
+  hour <- floor(hour) # Remove decimal from hour, e.g., 22
+  minute <- floor(minutesAsDecimal * 60) # 0.0728 --> 4.368 floored to 4
+  
+  xAxisDateTick <- hour+minutesAsDecimal
+  plot(data.frame(x=data[3],y=data[i]),pch=20,col=colores[i-3],lty=0,type="o",xlab=xal,ylab=header_type[i],
 	main=paste(header_type[i],header_units[i],header_stat[i],header_range[i],sep=" - "))
+  #axis.Date(1, at = seq(as.Date(
+  #cat(max(data[3]))
+  #cat("\n")
 }
 
 
